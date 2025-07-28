2fe757e6e0878e900d666672a7ef7640a9ff67fe
Rethvik <rethvikdoddapaneni3@gmail.com>
2025-07-28 08:14:50 +0530

    added logic to generate temp creds

--- aws_session.py
+++ aws_session.py
@@ -1,13 +1,23 @@
 import boto3
 from botocore.exceptions import ClientError
+import logging
 
+
+logger = logging.getLogger(__name__)
 class AwsSession:
-    def __init__(self,aws_access_key,aws_secret_key):
-        self._aws_access_key = aws_access_key
-        self._aws_secret_key = aws_secret_key
+    def __init__(self,AccessKeyId,SecretAccessKey,SessionToken,Expiration):
+        self._aws_access_key = AccessKeyId
+        self._aws_secret_key = SecretAccessKey
+        self._aws_session_token = SessionToken
+        self._expiration = Expiration
     def authenticate(self):
         try:
-            session = boto3.Session(aws_access_key_id=self._aws_access_key,aws_secret_access_key=self._aws_secret_key)
+            session = boto3.Session(
+                aws_access_key_id=self._aws_access_key,
+                aws_secret_access_key=self._aws_secret_key,
+                aws_session_token=self._aws_session_token)
             return session
         except ClientError as e:
-            print(e)
+            logger.error(e)
+        except Exception as e:
+            logger.exception(e)
--- script.py
+++ script.py
@@ -1,11 +1,26 @@
 import aws_session
 import os
-def run():
-    AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
-    AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
-    local_session = aws_session.AwsSession(AWS_ACCESS_KEY,AWS_SECRET_KEY)
-    aws_connection = local_session.authenticate()
-    s3_client = aws_connection.client('s3')
-    data = s3_client.list_buckets()
-    print(f'Buckets are {data["Buckets"]}')
-run()
+import logging
+import boto3
+from botocore.exceptions import ClientError
+
+logging.basicConfig(format='%(asctime)s || %(levelname)s || %(name)s || %(message)s')
+logger = logging.getLogger(__name__)
+
+def generate_temporary_creditianls():
+    try:
+        AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
+        AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
+        sts_client = boto3.client('sts',
+                                  aws_access_key_id=AWS_ACCESS_KEY,
+                                  aws_secret_access_key=AWS_SECRET_KEY)
+        response = sts_client.get_session_token(DurationSeconds=900)
+        sts_client.close()
+        aws_connection_init = aws_session.AwsSession(**response['Credentials'])
+        aws_connection_estab = aws_connection_init.authenticate()
+        s3_client = aws_connection_estab.client('s3')
+        buckets = s3_client.list_buckets()
+        print(f'Buckets are {buckets["Buckets"]}')
+    except ClientError as e: 
+        logger.exception(e)
+generate_temporary_creditianls()
